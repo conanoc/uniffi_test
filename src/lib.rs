48f8a89b55e1051be93d675f11e2f746c372958c
conanoc <conanoc@gmail.com>
2023-05-15 17:49:37 +0900

    Add Session, which fails to compile

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use sqlx::{
         SqliteAutoVacuum, SqliteConnectOptions, SqliteJournalMode, SqliteLockingMode,
         SqlitePoolOptions, SqliteSynchronous,
     },
-    migrate::MigrateDatabase, Sqlite, SqlitePool,
+    migrate::MigrateDatabase, Sqlite, SqlitePool, pool::PoolConnection,
 };
 
 uniffi::include_scaffolding!("bug_finder");
@@ -14,6 +14,10 @@ const DB_URL: &str = "sqlite:///tmp/test.db";
 
 pub struct Store {
     pool: RwLock<Option<SqlitePool>>,
+}
+
+pub struct Session {
+    connection: PoolConnection<Sqlite>,
 }
 
 #[uniffi::export(async_runtime = "tokio")]
@@ -50,15 +54,11 @@ pub async fn create_store() -> Arc<Store> {
 
 #[uniffi::export(async_runtime = "tokio")]
 impl Store {
-    pub async fn count(&self) -> u32 {
+    pub async fn session(&self) -> Arc<Session> {
         println!("In count");
         let pool = self.pool.read().await;
-        let mut conn = pool.as_ref().unwrap().acquire().await.unwrap();
-        let count: u32 = sqlx::query_scalar("SELECT COUNT(*) FROM users")
-            .fetch_one(&mut conn)
-            .await
-            .unwrap();
-        count
+        let conn = pool.as_ref().unwrap().acquire().await.unwrap();
+        Arc::new(Session { connection: conn })
     }
 
     pub async fn close(&self) {
@@ -67,6 +67,18 @@ impl Store {
         println!("before close");
         pool.unwrap().close().await;
         println!("after close");
+    }
+}
+
+#[uniffi::export(async_runtime = "tokio")]
+impl Session {
+    pub async fn count(&self) -> u32 {
+        println!("In count");
+        let count: u32 = sqlx::query_scalar("SELECT COUNT(*) FROM users")
+            .fetch_one(&mut self.connection)
+            .await
+            .unwrap();
+        count
     }
 }
 
@@ -79,8 +91,10 @@ mod tests {
     fn close_test() {
         future::block_on(async_compat::Compat::new(async {
             let store = create_store().await;
-            let count = store.count().await;
+            let session = store.session().await;
+            let count = session.count().await;
             println!("count: {}", count);
+            drop(session);
             store.close().await;
         }));
     }
